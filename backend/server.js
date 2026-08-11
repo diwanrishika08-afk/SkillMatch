28621be6abcbfa545429bb1513aa6a1cf1ea2142
Rishika Diwan <diwanrishika08@gmail.com>
2026-08-11 10:54:21 +0530

    Allow Netlify frontend

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,9 @@ const app = express();
 
 app.use(cors({
     origin: [
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
+             "http://127.0.0.1:5500",
+             "http://localhost:5500",
+             "https://skillmatch102.netlify.app"
     ]
 }));
 
